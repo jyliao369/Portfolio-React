606a15380de7b611b53fe1c68bfa9c27db653411
jyliao369 <jyliao369@gmail.com>
2022-03-24 19:00:46 -0400

    deploying

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div class="footer">
       <h1>Justin Liao</h1>
-      <div>© Copyright 2021. All right reserved</div>
+      <div>© Copyright 2021. All right reserve</div>
     </div>
   );
 };
